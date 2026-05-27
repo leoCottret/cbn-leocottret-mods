ec09e5c9e47a5318439eb0313134163bc9f5c7ac
leoCottret <71428793+leoCottret@users.noreply.github.com>
2026-05-27 12:09:41 +0200

    fix indentation for XE038 to match BN

--- MODS/XE038/preload.lua
+++ MODS/XE038/preload.lua
@@ -2,7 +2,7 @@ local mod = game.mod_runtime[game.current_mod]
 gdebug.log_info("XE038: preload online.")
 
 game.add_hook("on_character_effect", function(params)
-	if mod and mod.on_character_effect then
-		return mod.on_character_effect(params)
-	end
+  if mod and mod.on_character_effect then
+    return mod.on_character_effect(params)
+  end
 end)
